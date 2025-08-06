942ecde8bb74b5aa0d7320a6621f8764739fd0f8
yourneighborhoodealer-sys <yourneighborhoodealer@gmail.com>
2025-08-06 16:27:41 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import { UserProvider } from './src/context/UserContext';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
+  );
+}
